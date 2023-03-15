d34afc9b19bd96c6345b6eacef5ac0144886ad1f
LucasLutf <oolucaslutf@gmail.com>
2023-03-15 16:58:48 -0300

    Criação da API REST

--- livraria/models.py
+++ livraria/models.py
@@ -11,6 +11,7 @@ class Editora(models.Model):
     nome = models.CharField(max_length=100)
     site = models.URLField(null=True, blank=True)
 
+
     def __str__(self):
         return self.nome
     
@@ -32,6 +33,7 @@ class Livro(models.Model):
     quantidade = models.IntegerField(default=0)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros")
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros")
 
     def __str__(self):
         return f"{self.titulo} ({self.quantidade})"
